ec7d05232a67605e5a20422a2aae5f70e187b8cd
Vera E <97448213+ZenonCodes@users.noreply.github.com>
2022-04-11 22:04:25 -0400

    Remove excess New Lines

---
+++ src/main/java/JerkSON.java
@@ -0,0 +1,10 @@
+public class JerkSON {
+    private String name;
+    private String price;
+
+    public JerkSON(String name, String price){
+        this.name = name;
+        this.price = price;
+    }
+
+}
